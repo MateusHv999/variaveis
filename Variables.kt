d3c8ebdd153065488a8350d0dd8bfcdbf4238490
Mateus H <123660865+retroboy999@users.noreply.github.com>
2023-06-06 15:41:40 -0300

    Variables
    Variáveis mutáveis e imutáveis

---
+++ Variables.kt
@@ -0,0 +1,14 @@
+package com.example.variaveis
+
+class Variables {
+
+}
+
+fun main() {
+  var mutavel = 6
+  //val imutavel = 6
+  mutavel = 5
+  //imutavel = 7
+  println(mutavel) // retorna valor 5
+  //println(imutavel) retorna Val cannot be reassigned
+}
